b9f05b17805e0b3be8b28f9cd19ea5369d3ac974
Theo Richart <theo.richart@gmail.com>
2015-07-17 01:14:39 +0200

    init commit

---
+++ src/ScaffoldTest/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ScaffoldTest")>]
+[<assembly: AssemblyProductAttribute("ScaffoldTest")>]
+[<assembly: AssemblyDescriptionAttribute("test")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
